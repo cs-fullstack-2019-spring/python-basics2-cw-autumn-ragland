5e2d3628a5809c4bf0ade0bf4f807ecc9e990927
autumn-ragland <raglanda998@gmail.com>
2019-01-22 15:24:25 -0600

    graded classwork updated

--- CW.py
+++ CW.py
@@ -2,8 +2,8 @@ import random
 def main():
     # problem1()
     # problem2()
-    # problem3()
-    problem4()
+    problem3()
+    # problem4()
 # Create a random number. Print the number.
 def problem1():
     randomNum = random.randint(0, 10)
@@ -22,8 +22,9 @@ def problem3():
     userInput = ""
     while userInput != 0:
         userInput =int(input("Enter a positive number\n"))
-        for numbers in range(0, userInput+1):
-            print(numbers)
+        if userInput != 0:
+            for numbers in range(0, userInput+1):
+                print(numbers)
 # Create a function that creates a random number. Ask the user to guess the random number.
 # Keep letting the user guess until they get it right, then quit.
 # If they don't get it right, tell them if their next guess has to be higher or lower.
@@ -35,7 +36,7 @@ def problem4():
         userInput = int(input("Enter a number between 0 and 10\n"))
         if userInput < randomNum:
             print("guess higher")
-        elif userInput >randomNum:
+        elif userInput > randomNum:
             print("guess lower")
 
 if __name__ == '__main__':
